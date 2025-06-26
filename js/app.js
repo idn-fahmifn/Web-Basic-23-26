88af4f9bf2a91ef68857719bfd76f771e454e752
idn-fahmifn <fahmifn.idn@gmail.com>
2025-06-26 13:54:15 +0700

    4.5

---
+++ js/app.js
@@ -0,0 +1,59 @@
+
+
+// tipe data primitif
+
+// string
+// string wajib menggunakan kutip
+let belajar = 'web basic';
+let kata = `saya hari ini sedang belajar ${belajar}`;
+let today = belajar;
+
+// number
+// number tidak perlu tanda kutip
+let umur = 10;
+let bb = 70.3
+
+// boolean 
+// tipe data yang isinya hanya true / false
+// boolean tidak perlu menggunakan kutip
+
+let nikah = false;
+let makan = true;
+
+// undifined
+// gaperlu mengisi nilai.
+let cadangan;
+
+// null
+// nilainya adalah kosong
+let dompet = null;
+let inves = ""; //string null berbeda dengan null
+
+console.log(typeof nikah)
+
+
+
+
+
+// mendefinisikan variable
+
+let nama = "antoni";
+let nickname = "toni";
+const asal = "Jakarta";
+
+nama = "agus";
+
+// memanggil variable
+console.log('hallo, nama saya adalah ' + nama + ' saya biasa dipanggil'+nickname);  //nama saya antoni, saya biasa dipanggil toni.
+console.log('saya berasal dari '+asal); //saya berasal dari jakarta
+
+// template literal 
+console.log(`Hallo nama saya adalah ${nama}, saya biasa dipanggil ${nickname}, saya berasal dari ${asal}`);
+
+
+
+
+
+// console.log('saya sedang belajar javascript');
+// alert('Hallo saya sedang belajar javascript');
+// prompt('Berapa usia kamu?');
